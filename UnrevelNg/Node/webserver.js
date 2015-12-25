710e370fb224e65779ebea3213f9b5da336f7a48
Frank <tlan16@sina.com>
2015-12-25 19:18:50 +1100

    Exercise 1 finished

--- UnrevelNg/Node/webserver.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Frank-Desktop on 24/12/2015.
- */
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-app.use(serveStatic("../Work/UnraveNg"));
-app.listen(5100);
